2c95bfdb9ec96edb22321b93279ee094f2e86aa7
Fahad-Coder <36621752+Fahad-Coder@users.noreply.github.com>
2018-02-20 11:37:47 +0300

    initiation

--- app/src/main/java/com/seu/fitforlife/homepersonalcoach/LoginActivity.java
+++ app/src/main/java/com/seu/fitforlife/homepersonalcoach/LoginActivity.java
@@ -2,6 +2,7 @@ package com.seu.fitforlife.homepersonalcoach;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void Login (View v){
+
     }
 }
